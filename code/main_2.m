cf952982f0d400df85574691f270284b0b6db422
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-03-12 23:35:00 -0500

    Made changes to the files in code/ folder

--- code/main_2.m
+++ code/main_2.m
@@ -10,7 +10,7 @@ files_dir = "/mnt/rstor/CSE_BME_AXM788/data/TCGA_Ovarian Cancer/TCGA_Ovarian_Dia
 feature_maps_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/collagen_feature_maps_200/";
 files = dir(fullfile(files_dir, '*.svs'));
 feature_maps = dir(fullfile(feature_maps_dir, '*.mat'));
-collagen_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/collagen_feature_maps_200_final/";
+collagen_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/sample_1/";
 
 % hard-coded paths for masks and images
 %files_dir = "../../ovarian_cancer_files/";
@@ -29,21 +29,25 @@ for index = 1:length(files)
     file_feature_map = [];
     count = 0;
     sum = 0;
-    max = -1;
-    min = 1000000;
+    max_file = -1000000;
+    min_file = 1000000;
     for index1 = 1:length(feature_maps)
         file_feature_map_index1 = feature_maps(index1).name;
         file_feature_map_index1 = extractBefore(file_feature_map_index1, ".mat");
         file_feature_map_index1_split = split(file_feature_map_index1, "_");
 
         if strcmp(file_feature_map_index1_split{1}, filename)
-            count = count + 1;
             row = file_feature_map_index1_split(2);
             col = file_feature_map_index1_split(3);
             row = cellfun(@str2num, row);
             col = cellfun(@str2num, col);
             matrix = load(feature_maps_dir + file_feature_map_index1 + ".mat");
+            count = count + 1;
             sum = sum + mean(matrix.matrix, 'all', 'omitnan');
+            min_val = min(matrix.matrix, [], 'all', 'omitnan');
+            max_val = max(matrix.matrix, [], 'all', 'omitnan');
+            min_file = min(min_file, min_val);
+            max_file = max(max_file, max_val);
             size_matrix = size(matrix.matrix);
             row = ((row / 3000) * 20) + 1;
             col = ((col / 3000) * 20) + 1;
@@ -52,7 +56,10 @@ for index = 1:length(files)
     end
 
     % update zero values in feature map to nan
-    sum/count
+    mean_file = sum/count;
+    range_file = max_file - min_file;
+    feature_matrix = [mean_file, min_file, max_file, range_file];
+    csvwrite(collagen_masks_dir + filename + '.csv', feature_matrix);
     file_feature_map(file_feature_map == 0) = NaN;
     %save(collagen_masks_dir + filename + '.mat', "file_feature_map");
 
