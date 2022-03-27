445b29d8de7e301596277e56180120c2c07e7552
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-03-27 16:18:57 -0400

    Worked on extracting patches for Cervix Cancer

--- code/extract_patches.py
+++ code/extract_patches.py
@@ -57,6 +57,6 @@ def patch_extraction(wsi_path, output_path, tile_size=3000):
 
 # command to extract patches
 files = glob.glob(input_path + "*")
-files = files[240:]
+files = [input_path + 'TCGA-ZX-AA5X.svs']
 for file in files:
     patch_extraction(wsi_path=file, output_path=output_path)
