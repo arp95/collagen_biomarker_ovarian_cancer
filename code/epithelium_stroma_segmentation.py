a96cdb571133de87170a9b99a9d64790e3a8853f
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-03-31 12:55:13 -0400

    Worked on epi/stroma segmentation, modified files in code/ folder

--- code/epithelium_stroma_segmentation.py
+++ code/epithelium_stroma_segmentation.py
@@ -108,7 +108,7 @@ def save_patch_epithelium_stroma_mask(patch, output_path):
 # run code
 if __name__ == '__main__':
     patches = glob(input_path + "*")
-    patches = patches[71000:72000]
+    patches = patches[70000:71000]
     for patch in patches:
         filename = patch.split("/")[-1]
         print(filename)
