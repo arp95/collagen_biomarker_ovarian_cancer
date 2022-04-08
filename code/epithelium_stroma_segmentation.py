30827b3e36778d96544b15987c8de14f334ed8d1
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-08 11:16:38 -0400

    Started working on extracting patches for external validation dataset for OC

--- code/epithelium_stroma_segmentation.py
+++ code/epithelium_stroma_segmentation.py
@@ -108,7 +108,7 @@ def save_patch_epithelium_stroma_mask(patch, output_path):
 # run code
 if __name__ == '__main__':
     patches = glob(input_path + "*")
-    patches = patches[5000:10000]
+    patches = patches[10000:]
     for patch in patches:
         filename = patch.split("/")[-1]
         output_mask = get_patch_epithelium_stroma_mask(patch)
