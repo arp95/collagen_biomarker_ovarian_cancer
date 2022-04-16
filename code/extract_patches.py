1103c152991c89a142a0bb0074e5c54adf642b7f
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-16 10:42:31 -0400

    Worked on extracting patches for uh endometrial cases, modified files in code/ folder

--- code/extract_patches.py
+++ code/extract_patches.py
@@ -54,7 +54,7 @@ def patch_extraction(wsi_path, output_path, tile_size=3000):
 input_path = "/scratch/users/sxa786/uh_endometrium_cancer/files/"
 output_path = "/scratch/users/sxa786/uh_endometrium_cancer/patches/"
 files = glob.glob(input_path + "*")
-files = files[40:60]
+files = files[60:80]
 print(files)
 for file in files:
     print(file)
