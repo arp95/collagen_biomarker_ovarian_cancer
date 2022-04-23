440ed5de58d4fa3e03e2804e25df1497353dfcb3
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-22 21:34:58 -0400

    Worked on extracting patches for cervix cancer, modified files in code/ folder

--- code/extract_patches.py
+++ code/extract_patches.py
@@ -54,7 +54,7 @@ def patch_extraction(wsi_path, output_path, tile_size=3000):
 input_path = "/mnt/rstor/CSE_BME_AXM788/data/Gyn_TCGA/Cervix/"
 output_path = "/scratch/users/rnd27/tcga_cervix_cancer/patches/"
 files = glob.glob(input_path + "*")
-files = files[60:70]
+files = files[70:80]
 print(files)
 for file in files:
     print(file)
