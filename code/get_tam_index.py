8806642622ca79c7bb85ac3318dc83b2c4985256
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-05-07 15:35:18 -0400

    Added new files

--- code/get_tam_index.py
+++ code/get_tam_index.py
@@ -16,7 +16,7 @@ files = files[16]
 macrophage_masks = glob.glob(macrophage_masks_dir + "*")
 for file in files:
     filename = file.split("/")[-1][:-4]
-    filename = "TCGA-23-1116"
+    filename = "TCGA-OY-A56P"
     count_macrophage = 0.0
     count_total = 0.0
     count1_macrophage = 0.0
