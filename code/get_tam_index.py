b5f52825b937431799d544ff0ae834ac680ed1cb
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-05-07 13:52:10 -0400

    Added new files

--- code/get_tam_index.py
+++ code/get_tam_index.py
@@ -12,7 +12,7 @@ macrophage_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_canc
 output_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/macrophage_output_2/"
 
 files = glob.glob(files_dir + "*")
-files = files[96:]
+files = files[4:8]
 macrophage_masks = glob.glob(macrophage_masks_dir + "*")
 for file in files:
     filename = file.split("/")[-1][:-4]
