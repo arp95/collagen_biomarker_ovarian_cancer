78695ecad7ea18d8bac66f9497a56c0c83fd87e1
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-05-17 10:35:14 -0400

    Added new files

--- code/final_generate_macrophage_nuclei_mask.py
+++ code/final_generate_macrophage_nuclei_mask.py
@@ -5,12 +5,12 @@ import glob
 import csv
 
 
-macrophage_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/macrophage_nuclei_interim_masks/"
+macrophage_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/macrophage_nuclei_interim_masks_1/"
 nuclei_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/nuclei_masks/"
-output_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/macrophage_nuclei_masks/"
+output_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/macrophage_nuclei_masks_1/"
 
 masks = glob.glob(macrophage_masks + "*")
-masks = masks[30000:]
+masks = masks[:5000]
 for mask in masks:
     filename = mask.split("/")[-1]
     nuclei_mask = cv2.imread(nuclei_masks + filename, 0)
@@ -28,10 +28,10 @@ for mask in masks:
         area = cv2.contourArea(cnt)
         
         count1 = 0
-        for index1 in range(max(y-2, 0), min(y+h+2, 3000)):
-            for index2 in range(max(x-2, 0), min(x+w+2, 3000)):
+        for index1 in range(max(y-5, 0), min(y+h+5, 3000)):
+            for index2 in range(max(x-5, 0), min(x+w+5, 3000)):
                 if macrophage_mask[index1, index2] > 0:
                     count1 += 1
-        if count1 > 0.5*area:
+        if count1 > 0.25*area:
             cv2.fillPoly(final_mask, pts=[cnt], color=(255, 255, 255))
     cv2.imwrite(output_masks+filename, final_mask)
