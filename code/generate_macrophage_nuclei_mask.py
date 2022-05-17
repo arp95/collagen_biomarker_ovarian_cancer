e3a2dee14449bcfdad252a8c3701b7b647207f1c
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-05-16 22:27:43 -0400

    Added new files

--- code/generate_macrophage_nuclei_mask.py
+++ code/generate_macrophage_nuclei_mask.py
@@ -115,7 +115,7 @@ img_transform = torchvision.transforms.Compose([
 
 
 patches = glob.glob(patches_dir + "*")
-patches = patches[33500:]
+patches = patches[31000:32500]
 for patch in patches:
     filename = patch.split("/")[-1]
     image = Image.open(patch).convert('RGB')
