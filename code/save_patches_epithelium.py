b3e9bce824445e01663f46178453dbec0700123f
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-05-20 10:00:15 -0400

    Added new files

--- code/save_patches_epithelium.py
+++ code/save_patches_epithelium.py
@@ -5,9 +5,9 @@ import glob
 import csv
 
 
-patches = "/scratch/users/rnd27/tcga_ovarian_cancer/patches/"
-epi_stroma_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/epi_stroma_masks/"
-output_patches = "/scratch/users/rnd27/tcga_ovarian_cancer/patches_epithelium/"
+patches = "/scratch/users/axa1399/upmc_ovarian_cancer/patches/"
+epi_stroma_masks = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/epi_stroma_masks/"
+output_patches = "/scratch/users/axa1399/upmc_ovarian_cancer/patches_epithelium/"
 
 masks = glob.glob(patches + "*")
 masks = masks[:5000]
@@ -17,5 +17,5 @@ for mask in masks:
     epi_stroma_mask = cv2.imread(epi_stroma_masks + filename, 0)
 
     count = sum(sum(epi_stroma_mask > 0))
-    if float(count)/9000000 > 0.8:
+    if float(count)/9000000 > 0.75:
         cv2.imwrite(output_patches + filename, patch)
