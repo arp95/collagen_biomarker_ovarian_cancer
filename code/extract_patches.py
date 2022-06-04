5186f897258633bbd9fde959af6bebad64234aef
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-06-04 00:35:10 -0400

    Made changes

--- code/extract_patches.py
+++ code/extract_patches.py
@@ -56,7 +56,7 @@ output_path = "/scratch/users/axa1399/tcga_cervix_cancer/patches/"
 #input_path = "/scratch/users/sxb1208/tcia_ovarian_cancer/files/"
 #output_path = "/scratch/users/sxb1208/tcia_ovarian_cancer/patches/"
 files = glob.glob(input_path + "*")
-files = files[240:250]
+files = files[250:260]
 print(files)
 for file in files:
     print(file)
