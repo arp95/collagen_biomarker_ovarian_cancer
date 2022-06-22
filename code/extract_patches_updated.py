4351f0fe39f9a936402b76d6028507e180ee5833
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-06-22 18:31:34 -0400

    Made changes

--- code/extract_patches_updated.py
+++ code/extract_patches_updated.py
@@ -50,8 +50,8 @@ def patch_extraction(wsi_path, output_path, tile_size=3000):
 
 
 # command to extract patches
-input_path = "/mnt/rstor/CSE_BME_AXM788/data/Gyn_TCGA/Cervix/"
-output_path = "/scratch/users/axa1399/tcga_cervix_cancer/patches/"
+input_path = "/scratch/users/axa1399/tcia_ovarian_cancer/files/e/"
+output_path = "/scratch/users/axa1399/tcia_ovarian_cancer/patches/"
 files = glob.glob(input_path + "*")
 files = files[:10]
 print(files)
