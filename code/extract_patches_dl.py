ff005267922ee6a0994ba889fee559cd4130b169
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-06-26 11:40:11 -0400

    Made changes

--- code/extract_patches_dl.py
+++ code/extract_patches_dl.py
@@ -51,10 +51,10 @@ def patch_extraction(wsi_path, output_path, tile_size=512):
 
 
 # command to extract patches
-input_path = "/scratch/users/axa1399/dl_predict_outcome/validation/files/high/cervix/"
+input_path = "/scratch/users/axa1399/dl_predict_outcome/validation/files/high/endometrial/"
 output_path = "/scratch/users/axa1399/dl_predict_outcome/validation/input/high/"
 files = glob.glob(input_path + "*")
-files = files[20:]
+files = files[:10]
 print(files)
 for file in files:
     print(file)
