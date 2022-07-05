bde5b8426e52397301e0efbbaa8b99ac6115b86b
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-07-04 21:22:45 -0400

    Made changes

--- code/extract_new_input.py
+++ code/extract_new_input.py
@@ -6,9 +6,9 @@ import numpy as np
 
 
 # paths
-input_dir = glob.glob("/scratch/users/axa1399/dl_predict_outcome/validation/input/low/*")
-epi_stroma_dir = "/scratch/users/axa1399/dl_predict_outcome/validation/epi_stroma_masks/low/"
-new_input_dir = "/scratch/users/axa1399/dl_predict_outcome/validation/new_input/low/"
+input_dir = glob.glob("/scratch/users/axa1399/dl_predict_outcome/validation/input/high/*")
+epi_stroma_dir = "/scratch/users/axa1399/dl_predict_outcome/validation/epi_stroma_masks/high/"
+new_input_dir = "/scratch/users/axa1399/dl_predict_outcome/validation/new_input/high/"
 
 for index in range(0, len(input_dir)):
     input_filename = input_dir[index].split("/")[-1]
