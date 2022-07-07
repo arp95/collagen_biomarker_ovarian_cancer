06884804018d9928a722b0705ef5b5e775cc9a12
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-07-07 08:30:55 -0400

    Made changes

--- code/extract_new_input.py
+++ code/extract_new_input.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # paths
 input_dir = glob.glob("/scratch/users/axa1399/dl_predict_outcome/test/input/high/*")
-input_dir = input_dir[150000:175000]
+input_dir = input_dir[175000:200000]
 epi_stroma_dir = "/scratch/users/axa1399/dl_predict_outcome/test/epi_stroma_masks/high/"
 new_input_dir = "/scratch/users/axa1399/dl_predict_outcome/test/new_input/high/"
 
